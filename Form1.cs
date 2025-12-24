f912419d2943f5f8649a75649d369a496e00f116
Oleg <Oleg@DESKTOP-VQGUHUC>
2025-12-24 03:54:31 +0200

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Newtonsoft.Json; 
+
+namespace JsonManagerApp
+{
+    public partial class Form1 : Form
+    {
+        List<Book> _books = new List<Book>();
+        string _filePath = "library.json";
+
+        public Form1()
+        {
+            InitializeComponent(); 
+
+            button1.Click += (s, e) => OpenData();    
+            button2.Click += (s, e) => SaveData();    
+            button3.Click += (s, e) => AddBook();     
+            button4.Click += (s, e) => MessageBox.Show("Виконав: Чубко Олег\nГрупа: К-26");
+            textBox1.TextChanged += (s, e) => Search(); 
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void AddBook()
+        {
+            _books.Add(new Book { Title = "Нова книга", Author = "Автор", Year = 2025 });
+            RefreshGrid(_books);
+        }
+
+        private void SaveData()
+        {
+            File.WriteAllText(_filePath, JsonConvert.SerializeObject(_books));
+            MessageBox.Show("Збережено у файл!");
+        }
+
+        private void OpenData()
+        {
+            if (File.Exists(_filePath))
+            {
+                _books = JsonConvert.DeserializeObject<List<Book>>(File.ReadAllText(_filePath));
+                RefreshGrid(_books);
+            }
+        }
+
+        private void Search()
+        {
+     
+            string text = textBox1.Text.ToLower();
+            var filtered = _books.Where(b => b.Title.ToLower().Contains(text)).ToList();
+            RefreshGrid(filtered);
+        }
+
+        private void RefreshGrid(List<Book> list)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = list;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("JsonManagerApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JsonManagerApp")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9155245e-2a5c-4f8e-ab04-9cd98b337400")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
